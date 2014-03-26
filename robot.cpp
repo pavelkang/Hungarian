02e34816bdee56d41b65ae8f6a61c8e5209dc724
pavelkang <sclingtt@gmail.com>
2014-03-26 01:19:22 -0400

    v1.1

--- robot.cpp
+++ robot.cpp
@@ -9,18 +9,19 @@ void AddToTree(int, int);
 void UpdateLabels();
 
 int cost[N][N];  // cost matrix
-int num_robots = N;
-int num_jobs   = N;
-int max_match  = 0;
+int num_robots = N;  // number of rows
+int num_jobs   = N;  // number of cols
+int max_match  = 0;  // if max_match is N, we are done
 int label_x[N], label_y[N];  // vertex labels
-int xy[N];  // xy[x] - vertex matched with x
-int yx[N];  // yx[x] - vertex matched with y
-bool S[N], T[N];  // Alternating tree
-int slack[N];
+int xy[N];  // xy[x] - vertex in Y matched with x
+int yx[N];  // yx[x] - vertex in X matched with y
+bool S[N], T[N];  // Helps find augmenting path
+int slack[N];  // helps update label
 int slackx[N]; // slack[y] -> x such that l(x) + l(y) - w(x,y) = slack[y]
 int prev[N];  // for memorizing alternating paths
 
 // Step 0 Label
+// empty matching
 void InitLabels() {
   memset(label_x, 0, sizeof(label_x));
   memset(label_y, 0, sizeof(label_y));
@@ -42,16 +43,19 @@ void Augment() {
   memset(prev, -1, sizeof(prev)); // init prev for the alternating tree
   // -1 means no prev. -2 means current is root
 
+  // find ONE unmatched x and make it root
   for (x=0; x<num_robots; x++) {
     // finding the exposed x in X and make it the root of the alternating tree
     if (xy[x] == -1) {
-      q[wr++] = root = x;  // find root of the tree
+      // if x is unmatched
+      q[wr++] = root = x;  // find roots of the tree
       prev[x] = -2;  // root has no prev
       S[x] = true;   // put x in S
       break;
     }
   }
 
+  // initialize slack to save computations
   for (y=0; y<num_robots; y++) {
     // Now T is empty, so we find all the slacks of y in Y
     slack[y] = label_x[root] + label_y[y] - cost[root][y]; // help find its min value
@@ -62,26 +66,30 @@ void Augment() {
   while (true) {
     // look for augmenting path
     while (rd < wr) {
+      // iterating over all possible roots
       x = q[rd++];  // one root.   q is for building tree with bfs search
       for (y=0; y<num_robots; y++) {
 	if (cost[x][y] == label_x[x] + label_y[y] && !T[y]) { // for all edges
 	  if (yx[y] == -1) break;  // an exposed vertex in Y found. augmenting path.
 	  T[y] = true;  // add y to T
-	  q[wr++] = yx[y];  // ???
-	  AddToTree(yx[y], x);  // put yx[y] in S because
+	  q[wr++] = yx[y];  //
+	  AddToTree(yx[y], x);  // put yx[y] in S because y is matched to yx[y]
 	}
       }
       if (y<num_robots) break;
     }
     if (y<num_robots) break;
 
+    // Execute the following code only if we fail to find an augmenting path
+
     UpdateLabels();
     wr = rd = 0;
 
+    // We have updated the labels, but we still need to add edges to the tree
     // y is the newly added element to improve the labeling
     for (y=0; y<num_robots; y++) {
-      if (!T[y] && slack[y] == 0) {
-	if (yx[y] == -1) {  // not connected
+      if (!T[y] && slack[y] == 0) {  // augmenting path
+	if (yx[y] == -1) {  // not matched
 	  x = slackx[y];
 	  break;
 	}
@@ -98,10 +106,10 @@ void Augment() {
     if (y < num_robots) break;
   }
 
-  if (y<num_robots) {  // augmenting path found
+  // augmenting path found, reverse all edges along the path
+  if (y<num_robots) {
     max_match++;  // increment
-    // reverse every edge in the augmenting path
-    // y is the unconnected vertix, the target of the augmenting path
+    // y is the unconnected vertex, the target of the augmenting path
     // x is slackx[y]
     // we traverse backward until cx == -2 which is the root
     for (int cx=x, cy=y, ty; cx != -2; cx = prev[cx], cy =ty) {
@@ -128,6 +136,7 @@ void UpdateLabels() {
       slack[y] -= delta;  // This is because l(x) is delta less, l(y) is constant
 }
 
+
 void AddToTree(int x, int prevx) {
   // x     - current vertex
   // prevx - vertext from X before x in the alternating path
@@ -136,6 +145,7 @@ void AddToTree(int x, int prevx) {
   for (int y=0; y<num_robots; y++) {
     if (label_x[x] + label_y[y] - cost[x][y] < slack[y]) {
       // update slack because we added a new element to S
+      // because slack[y] will always be the minimum from y to ANY element x in X
       slack[y]  = label_x[x] + label_y[y] - cost[x][y];
       slackx[y] = x;
     }
